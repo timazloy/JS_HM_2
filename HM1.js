6034a095ea6df54f3d2a93bf0b070e79e8009c36
Tima <timazloy04444@gmail.com>
2021-09-17 18:54:12 +0300

    Add homework

---
+++ HM1.js
@@ -0,0 +1,28 @@
+'use strict'
+const products = [
+    { id: 1, title: 'Notebook', price: 1000 },
+    { id: 2, title: 'Mouse', price: 100 },
+    { id: 3, title: 'Keyboard', price: 250 },
+    { id: 4, title: 'Gamepad', price: 150 },
+];
+
+const renderProduct = (title, price, img = "http://placehold.it/150x200") => {
+    return `<div class="product-item">
+              <img style="margin-top: 20px" src="${img}" alt="img">
+              <h3>${title}</h3>
+              <p>${price}</p>
+              <button class="by-btn">Добавить</button>
+            </div>`;
+};
+
+const renderProducts = (list) => {
+    const productList = list.map((item) => {
+        return renderProduct(item.title, item.price);
+    }).join('')
+
+    console.log(productList);
+    document.querySelector('.products').innerHTML = productList;
+};
+
+renderProducts(products);
+
